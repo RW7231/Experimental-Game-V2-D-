9b57944249ca304d04744747d8ca91125811b2f5
RW7231 <154094370+RW7231@users.noreply.github.com>
2024-05-15 19:20:45 -0500

    Wall Texture

--- Scripts/OverworldMap.gd
+++ Scripts/OverworldMap.gd
@@ -3,6 +3,7 @@ extends Node2D
 # we will use the gridobject to represent an overworld and a playerobject for the player
 var GridObject = preload("res://Prefabs/grid_placeholder.tscn")
 var PlayerObject = preload("res://Prefabs/player.tscn")
+var WallObject = preload("res://Prefabs/wall.tscn")
 
 var size = 5
 
@@ -12,6 +13,8 @@ var player
 
 var Gridmin = 0
 var Gridmax = 64
+
+var playerpos = [3, 3]
 
 # we can use a 2d array to create a digital representation of the world map
 func make2dArray():
@@ -25,8 +28,17 @@ func make2dArray():
 	# we can then fill the array with the world grid
 	for i in size:
 		for j in size:
-			# to actually place the grid object in the world, instantiate it
-			var gridpiece = GridObject.instantiate()
+			
+			# initialize the gridpiece
+			var gridpiece
+			
+			# generate a random number between 0 and 99
+			# if it is less than 10, generate a wall, otherwise generate a blank space
+			if (randi() % 100) < 10:
+				gridpiece = WallObject.instantiate()
+			else:
+				gridpiece = GridObject.instantiate()
+				
 			# from there, change its position in the world.
 			# grid pieces are 16x16 pixels in size, multiply i and j by that
 			gridpiece.position = Vector2(i*16, j*16)
@@ -43,22 +55,28 @@ func _input(event):
 	if event.is_action_pressed("Up"):
 		if player.position.y > Gridmin:
 			player.position += Vector2(0, -16)
+			playerpos = [playerpos[0], playerpos[1] + 1]
 		
 	if event.is_action_pressed("Down"):
 		if player.position.y < Gridmax:
 			player.position += Vector2(0, 16)
+			playerpos = [playerpos[0], playerpos[1] - 1]
 		
 	if event.is_action_pressed("Left"):
 		if player.position.x > Gridmin:
 			player.position += Vector2(-16, 0)
+			playerpos = [playerpos[0] - 1, playerpos[1]]
 		
 	if event.is_action_pressed("Right"):
 		if player.position.x < Gridmax:
 			player.position += Vector2(16, 0)
+			playerpos = [playerpos[0] + 1, playerpos[1]]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# consistent generation
+	seed(1)
 	worldGrid = make2dArray()
 	player = PlayerObject.instantiate()
 	player.position = Vector2(32, 32)
